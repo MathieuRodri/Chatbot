95ff42ede5204fa60c4bbe54680aed8561c186bc
Mathieu RODRIGUES <mathieudomingues925@gmail.com>
2023-12-27 19:01:49 +0100

    update

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-import streamlit as st
-from dotenv import load_dotenv
+import os
 import pickle
 import numpy as np
 from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 import faiss
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
-import os
+from transformers import pipeline
 
-# Sidebar contents
-with st.sidebar:
-    st.image(os.path.join('assets', 'logo.png'), width = 100)
-    add_vertical_space(2)
-    st.image(os.path.join('assets', 'valeo_logo.png'), width = 200)
-    add_vertical_space(1)
-    st.title("Chatbot💬 - Contenus de Livres PDF")
-    st.markdown('''
-    ## Consignes
-    En utilisant LLM, développer un chatbot qui interagit avec un livre pdf, pour répondre
-    aux questions des utilisateurs, et les réponses sont basées sur le contenu du livre.
-    ''')
-    add_vertical_space(1)
-    st.write('Conception: Mathieu RODRIGUES 🧑‍💻')
-
+# Chargement des variables d'environnement
 load_dotenv()
 
 def create_faiss_index(embeddings):
+    """
+    Crée et retourne un index FAISS à partir des embeddings donnés.
+
+    Args:
+    embeddings (List[np.array]): Liste des embeddings (vecteurs de caractéristiques).
+
+    Returns:
+    faiss.IndexFlatL2: Index FAISS pour la recherche de proximité.
+    """
     dimension = embeddings[0].shape[0]
     index = faiss.IndexFlatL2(dimension)
     embeddings_np = np.array(embeddings).astype('float32')
@@ -36,37 +31,55 @@ def create_faiss_index(embeddings):
 
 def extract_full_sentence(response, context):
     """
-    Extracts a full sentence containing the response from the context.
+    Extrait une phrase complète contenant la réponse du contexte.
+
+    Args:
+    response (dict): Réponse du modèle de question-réponse.
+    context (str): Texte d'où la réponse a été extraite.
+
+    Returns:
+    str: Phrase complète contenant la réponse.
     """
     start_idx = response['start']
     end_idx = response['end']
 
-    # Expand to the nearest sentence boundaries
+    # Expansion aux limites les plus proches de la phrase
     sentence_start = context.rfind('.', 0, start_idx) + 1
     sentence_end = context.find('.', end_idx)
-
-    if sentence_end == -1:
-        sentence_end = len(context)
-    else:
-        sentence_end += 1
+    sentence_end = sentence_end + 1 if sentence_end != -1 else len(context)
 
     return context[sentence_start:sentence_end].strip()
 
 def main():
-    st.header("Chatbot 💬")
+    # Configuration et contenu de la barre latérale
+    with st.sidebar:
+        st.image(os.path.join('assets', 'logo.png'))
+        add_vertical_space(2)
+        st.image(os.path.join('assets', 'valeo_logo.png'))
+        add_vertical_space(1)
+        st.title("Chatbot💬 - Contenus de Livres PDF")
+        st.markdown('''
+        ## Consignes
+        En utilisant LLM, développer un chatbot qui interagit avec un livre pdf, pour répondre
+        aux questions des utilisateurs, et les réponses sont basées sur le contenu du livre.
+        ''')
+        add_vertical_space(1)
+        st.write('Conception: Mathieu RODRIGUES 🧑‍💻')
 
+    st.header("Chatbot (english only)💬")
+
+    # Modèle pour les embeddings
     embeddings_model = SentenceTransformer('all-MiniLM-L6-v2')
 
-    # upload a PDF file
+    # Téléversement d'un fichier PDF
     pdf = st.file_uploader("Téléverser (PDF)", type='pdf')
 
     if pdf is not None:
+        # Lecture et extraction du texte du PDF
         pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text() if page.extract_text() else ''
+        text = "".join([page.extract_text() or '' for page in pdf_reader.pages])
 
+        # Division du texte en morceaux pour le traitement
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200,
@@ -75,12 +88,10 @@ def main():
         chunks = text_splitter.split_text(text=text)
 
         with st.chat_message("human", avatar='🧑‍💻'):
-            st.write(chunks[0]+ " (...)")
+            st.write(chunks[0] + " (...)")
 
-        # Embeddings
+        # Gestion des embeddings et de l'index FAISS
         store_name = pdf.name[:-4]
-        #st.write(f'{store_name}')
-
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
                 index = pickle.load(f)
@@ -90,64 +101,97 @@ def main():
             with open(f"{store_name}.pkl", "wb") as f:
                 pickle.dump(index, f)
 
-        # Accept user questions/query
+        # Interaction avec l'utilisateur pour les questions
         query = st.chat_input("Ask questions about your PDF file:")
 
         if query:
             with st.chat_message("human", avatar='🧑‍💻'):
                 st.write(query)
 
-            with st.chat_message("ai", avatar='🤖'):
-                st.write("Let me some time to think...")
-                loading_chat = st.empty()
-                loading_chat.image(os.path.join('assets', 'loading.gif'))
+            # Recherche et réponse au chat
+            handle_chat_response(query, chunks, embeddings_model, index)
 
-            # Converting query to embedding
-            query_embedding = embeddings_model.encode(query, show_progress_bar=False)
-            query_embedding_np = np.array(query_embedding).reshape(1, -1).astype('float32')
+def handle_chat_response(query, chunks, embeddings_model, index):
+    """
+    Gère la réponse au chat en fonction de la requête de l'utilisateur.
 
-            # Performing the search
-            D, I = index.search(query_embedding_np, k=3)
+    Args:
+    query (str): Question de l'utilisateur.
+    chunks (List[str]): Morceaux de texte du PDF.
+    embeddings_model (SentenceTransformer): Modèle pour générer des embeddings.
+    index (faiss.IndexFlatL2): Index FAISS pour la recherche de proximité.
+    """
+    with st.chat_message("ai", avatar='🤖'):
+        st.write("Let me some time to think...")
+        loading_chat = st.empty()
+        loading_chat.image(os.path.join('assets', 'loading.gif'))
 
-            # Extracting the chunks corresponding to the indices
-            docs = [chunks[i] for i in I[0]] 
+    # Conversion de la requête en embedding et recherche
+    query_embedding = embeddings_model.encode(query, show_progress_bar=False)
+    query_embedding_np = np.array(query_embedding).reshape(1, -1).astype('float32')
+    D, I = index.search(query_embedding_np, k=3)
+    docs = [chunks[i] for i in I[0]]
 
-            # BERT QA pipeline
-            qa_pipeline = pipeline("question-answering", model="bert-large-uncased-whole-word-masking-finetuned-squad")
+    # Pipeline de question-réponse BERT
+    qa_pipeline = pipeline("question-answering", model="bert-large-uncased-whole-word-masking-finetuned-squad")
+    best_score, best_response, full_sentence, best_context = find_best_response(docs, qa_pipeline, query)
 
-            best_score = float('-inf')
-            best_response = None
-            full_sentence = ""
-            best_context = ""
+    # Affichage de la réponse rapide
+    loading_chat.empty()
+    with st.chat_message("assistant", avatar='🤖'):
+        st.write("**Quick Answer:", best_response + "**")
 
-            # Iterate through the responses to find the one with the highest score
-            for doc in docs:
-                response = qa_pipeline(question=query, context=doc)
-                if response['score'] > best_score:
-                    best_score = response['score']
-                    best_response = response['answer']
-                    full_sentence = extract_full_sentence(response, doc)
-                    best_context = doc
+    # Génération d'une réponse avancée
+    generate_advanced_response(query, best_context)
 
-            loading_chat.empty() 
-            with st.chat_message("assistant", avatar='🤖'):
-                st.write("**Quick Answer:", best_response+"**")
+def find_best_response(docs, qa_pipeline, query):
+    """
+    Trouve la meilleure réponse parmi les documents donnés en utilisant la pipeline QA.
 
-            with st.chat_message("ai", avatar='🤖'):
-                st.write("I'm now trying to generate an answer with a generative text model...")
-                loading_chat2 = st.empty()
-                loading_chat2.image(os.path.join('assets', 'loading.gif'))
+    Args:
+    docs (List[str]): Documents parmi lesquels chercher la réponse.
+    qa_pipeline (transformers.Pipeline): Pipeline de question-réponse.
+    query (str): Question posée.
 
-            text_generator = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
+    Returns:
+    Tuple[float, str, str, str]: Meilleur score, meilleure réponse, phrase complète, meilleur contexte.
+    """
+    best_score = float('-inf')
+    best_response = None
+    full_sentence = ""
+    best_context = ""
 
-            prompt = f"{query}\n\nContext:\n{best_context[:1000]}.\nKnowing that, i will answer to the question \"{query}\": "  # You can adjust the context length
-            response = text_generator(prompt, max_length=50 + len(prompt), num_return_sequences=1)
-            response = response[0]['generated_text'].split(f'Knowing that, i will answer to the question \"{query}\": ')[1]
+    for doc in docs:
+        response = qa_pipeline(question=query, context=doc)
+        if response['score'] > best_score:
+            best_score = response['score']
+            best_response = response['answer']
+            full_sentence = extract_full_sentence(response, doc)
+            best_context = doc
 
-            loading_chat2.empty()
-            with st.chat_message("assistant", avatar='🤖'):
-                st.write("Advanced Answer:", response)
-                #st.write("Full Sentence:", full_sentence)
+    return best_score, best_response, full_sentence, best_context
+
+def generate_advanced_response(query, best_context):
+    """
+    Génère une réponse avancée en utilisant un modèle de génération de texte.
+
+    Args:
+    query (str): Question posée.
+    best_context (str): Meilleur contexte trouvé pour la réponse.
+    """
+    with st.chat_message("ai", avatar='🤖'):
+        st.write("I'm now trying to generate an answer with a generative text model...")
+        loading_chat2 = st.empty()
+        loading_chat2.image(os.path.join('assets', 'loading.gif'))
+
+    text_generator = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
+    prompt = f"{query}\n\nContext:\n{best_context[:1000]}.\nKnowing that, I will answer to the question \"{query}\": "  # Longueur du contexte ajustable
+    response = text_generator(prompt, max_length=50 + len(prompt), num_return_sequences=1)
+    response = response[0]['generated_text'].split(f'Knowing that, I will answer to the question \"{query}\": ')[1]
+
+    loading_chat2.empty()
+    with st.chat_message("assistant", avatar='🤖'):
+        st.write("Advanced Answer:", response)
 
 if __name__ == '__main__':
     main()
